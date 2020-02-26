a48360309e146a70bdf3b6a83e21eccc60b7e203
Nidhhoggr <persie.joseph@gmail.com>
2020-02-26 10:16:43 +0000

    Adding jouhikko

---
+++ javascript/instruments.jouhiko.js
@@ -0,0 +1,26 @@
+var instruments = {
+  "cgcg": {
+    "name": "c'.g.c'.g''",
+    "numStrings": 4,
+    "fretsToLabel": [1, 3, 5, 7, 10],
+    "strings": ["G","C","G","C"]
+  },
+  "dgdg": {
+    "name": "d'.g.d'.g''",
+    "numStrings": 4,
+    "fretsToLabel": [1, 3, 5, 7, 10],
+    "strings": ["G","D","G","D"]
+  },
+  "aeae": {
+    "name": "a'.e.a'.e''",
+    "numStrings": 4,
+    "fretsToLabel": [1, 3, 5, 7, 10],
+    "strings": ["E","A","E","A"]
+  },
+  "trollstemt": {
+    "name": "trollstemt (a'.c#.a'.e'')",
+    "numStrings": 4,
+    "fretsToLabel": [1, 3, 5, 7, 10],
+    "strings": ["E","A","C#","A"]
+  }
+};
