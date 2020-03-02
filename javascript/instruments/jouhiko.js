cd3c7a1f374738c2012da8ce4c8afd305610f9a7
ValiantAnu <root@valiant-anu.kom>
2020-03-02 13:43:51 +0000

    Fixes octave notation

--- javascript/instruments/jouhiko.js
+++ javascript/instruments/jouhiko.js
@@ -1,30 +1,30 @@
 var instruments = {
   "cgcg": {
-    "name": "c'.g.c'.g''",
+    "name": "c'.g\".c'.g",
     "numStrings": 4,
     "fretsToLabel": [1, 3, 5, 7, 10],
     "strings": ["G","C","G","C"]
   },
   "dgdg": {
-    "name": "d'.g.d'.g''",
+    "name": "d'.g\".d'.g",
     "numStrings": 4,
     "fretsToLabel": [1, 3, 5, 7, 10],
     "strings": ["G","D","G","D"]
   },
   "aeae": {
-    "name": "a'.e.a'.e''",
+    "name": "a'.e\".a'.e",
     "numStrings": 4,
     "fretsToLabel": [1, 3, 5, 7, 10],
     "strings": ["E","A","E","A"]
   },
   "aeae": {
-    "name": "b'.e.b'.e''",
+    "name": "b'.e\".b'.e",
     "numStrings": 4,
     "fretsToLabel": [1, 3, 5, 7, 10],
     "strings": ["E","B","E","B"]
   },
   "trollstemt": {
-    "name": "trollstemt (a'.c#.a'.e'')",
+    "name": "trollstemt (a'.c#\".a'.e)",
     "numStrings": 4,
     "fretsToLabel": [1, 3, 5, 7, 10],
     "strings": ["E","A","C#","A"]
