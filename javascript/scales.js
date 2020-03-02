6f37be9fc4877dcc1bb4dd298556b3c51a2e0f39
ValiantAnu <root@valiant-anu.kom>
2020-03-02 13:04:21 +0000

    seperating scales in its own file

---
+++ javascript/scales.js
@@ -0,0 +1,62 @@
+var scales = {
+  "major": {
+    "name": "major",
+    "pattern": "o-o-oo-o-o-o"
+  },
+  "minor_melodic": {
+    "name": "melodic minor",
+    "pattern": "o-oo-o-o-o-o"
+  },
+  "minor_harmonic": {
+    "name": "harmonic minor",
+    "pattern": "o-oo-o-oo--o"
+  },
+  "minor_natural": {
+    "name": "natural minor",
+    "pattern": "o-oo-o-oo-o-"
+  },
+  "pentatonic_major": {
+    "name": "pentatonic (major)",
+    "pattern": "o-o-o--o-o--"
+  },
+  "pentatonic_minor": {
+    "name": "pentatonic (minor)",
+    "pattern": "o--o-o-o--o-"
+  },
+  "blues_major": {
+    "name": "blues (major)",
+    "pattern": "o-ooo--o-o--"
+  },
+  "blues_minor": {
+    "name": "blues (minor)",
+    "pattern": "o--o-ooo--o-"
+  },
+  "ionian_mode": {
+    "name": "ionian mode",
+    "pattern": "o-o-oo-o-o-o"
+  },
+  "dorian_mode": {
+    "name": "dorian mode",
+    "pattern": "o-oo-o-o-oo-"
+  },
+  "phrygian_mode": {
+    "name": "phrygian mode",
+    "pattern": "oo-o-o-oo-o-"
+  },
+  "lydian_mode": {
+    "name": "lydian mode",
+    "pattern": "o-o-o-oo-o-o"
+  },
+  "mixolydian_mode": {
+    "name": "mixolydian mode",
+    "pattern": "o-o-oo-o-oo-"
+  },
+  "aeolian_mode": {
+    "name": "aeolian mode",
+    "pattern": "o-oo-o-oo-o-"
+  },
+  "locrian_mode": {
+    "name": "locrian mode",
+    "pattern": "oo-o-oo-o-o-"
+  }
+};
